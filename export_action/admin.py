cbc03be67011b8fe671196851539e9dbb7efff42
sergii-shcherbak <33931306+sergii-shcherbak@users.noreply.github.com>
2018-07-18 16:00:56 +0200

    Django 2.0

--- export_action/admin.py
+++ export_action/admin.py
@@ -1,7 +1,7 @@
 import uuid
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 
@@ -18,7 +18,6 @@ def export_selected_objects(modeladmin, request, queryset):
     else:
         return HttpResponseRedirect(
             "%s?ct=%s&ids=%s" % (url, ct.pk, ",".join(str(pk) for pk in selected)))
-
 
 export_selected_objects.short_description = _("Export selected items...")
 
